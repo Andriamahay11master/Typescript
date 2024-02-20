fb5acdc5646435db1b38a0f76c1782c29ed510ba
Andriamahay11master <hirimanana@yahoo.fr>
2024-02-20 14:13:29 +0300

    chapter fonctions sous typescript

--- src/index.ts
+++ src/index.ts
@@ -1,49 +1,19 @@
-const test: string = "test";
-const foo: string = "une string"; // 'une string' // `une ${ 'string' }`	
-const bar: number = 10; //5.5 // -1.2 // 10e2
-const boolean: boolean = true // false // 1 ===1;
-
-const obj: object = {
-    name: 'jean'
-} //new Map() // []
-
-const anything: any = {};
-const arr: Array<string> = ['1', '2', '3', '4']; //ddclaration de tableau
-const arr2: string[] = ['1', '2', '3', '4'];//declaration de tableau
-const tuple: [string, number] = ['1', 2];//tableaux avec un nombre prédéfinis d'élèments
-
-
-//type enum
-enum XhrReadyState {
-    UNSENT,
-    OPENED,
-    HEADERS_RECEIVED,
-    LOADING,
-    DONE
-}
-
-const foo2 : null = null;
-const foo3 : undefined = undefined;
-
+//declaration base function in typescript
 function add(nbr1 : number, nbr2 : number) : number {
     return nbr1 + nbr2;
 }
 
-function loop() : never {
-    while(true) {
-        
-    }
+let add2 = (...number: Array<number>) => {
+    return number.reduce((total, nbr) => total + nbr, 0);
 }
 
-//inférence et assertion de types
-// Type inference and type assertion examples
+//surcharge de fonction 
+function addMulti(a: number, b: number) : number;
+function addMulti(a: string, b: string) : string;
+function addMulti(a, b) : number | string {
+    return a+b;
+}
 
-let num = 10; // Type inference: num is inferred as number
-let str = "hello"; // Type inference: str is inferred as string
-
-let numStr = num as unknown as string; // Type assertion: converting num to string
-let strNum = str as unknown as number; // Type assertion: converting str to number
-//
-console.log(XhrReadyState.DONE);
-console.log(XhrReadyState[0]);
-console.log(test);
+console.log(add2(10,20,5,30));
+console.log(addMulti(10,20));
+console.log(addMulti("Hello", "World"));

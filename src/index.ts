cef1c00840b3a87b5e3f8c4d6275cdf83ef5e88e
Andriamahay11master <hirimanana@yahoo.fr>
2024-02-22 09:27:14 +0300

    init unionType

--- src/index.ts
+++ src/index.ts
@@ -1,63 +1,23 @@
+//union de type
+let test : string | number; 
 
+function ajouter(a: string | number, b: string | number) {
+    return Number(a) + Number(b);
+  }
 
-interface User{
-    username: string,
-    isDrinking: boolean,
-    isHealthy?: boolean,
-    isSmooking?: boolean,
-    age: number
-    [propName: string]: any; //index
-}
+const ret = ajouter(10, "10")
+console.log(ret);
 
-const user : User = {
-    username: 'John',
-    isDrinking: true,
-    isHealthy: true,
-    isSmooking: true,
-    age: 20,
-    hairColor: 'black'
-}
-function greet(user: User) : void{
-    console.log(`Hello ${user.username}`)	
-}
-
-function death(user: User) : void{
-    
-}
-
-function goodShape(user: User) : void{
-    
-} 
-greet(user)
-
-const tableNotEditing: readonly string[] = ['a', 'b', 'c'];
-const tabNotEditing : ReadonlyArray<string> = ['a', 'b', 'c'];
-
-interface MyFunc{
-    (a: string, b: number): number
-}
-
-const func2 : MyFunc = (a: string, b: number) => a.length
-
-
-//composition d'interfaces
-interface Vehicule{
-    name: string,
-    drive : () => void
-}
-
-interface Engine{
-    type: string,
-}
-/*interface Car extends Vehicule, Engine{
-    wheels: number
-}*/
-
-class Car implements Vehicule, Engine{
-    drive: () => void;
-    constructor(public name: string, public wheels: number, public type: string){
-        this.drive = () => {}
-    }
-
-}
-
+//union d'interface
+interface User {
+    name: string;
+    email: string;
+  }
+  
+  interface Moderator extends User {
+    role: 'moderator';
+    editMessage: (msg: string) => string
+  }
+  
+  let user: User | Moderator | null;
+  user.email = "test@gmail.com";
